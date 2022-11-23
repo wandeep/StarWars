189bbb5ce238b72f233a7719c4b144207a364914
Wandeep Basra <wandeep@wandeep.com>
2022-11-23 14:50:30 +0000

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Wandeep Basra on 23/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
